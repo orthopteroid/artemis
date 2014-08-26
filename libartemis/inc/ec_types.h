e26bdc1feed39302b14ea3ca6bc70f44e8efc52d
john howard <orthopteroid@gmail.com>
2014-08-25 20:16:55 -0700

    typedef tidy: int is 32 bits under msvc x64/x32 and gcc x64.

--- libartemis/inc/ec_types.h
+++ libartemis/inc/ec_types.h
@@ -3,16 +3,7 @@
 
 typedef unsigned char	byte;	/*  8 bit */
 typedef unsigned short	word16;	/* 16 bit */
-
-#if defined(_WIN32)
-
-	typedef unsigned long	word32;	/* 32 bit */
-
-#else
-
-	typedef unsigned int	word32;	/* 32 bit */
-
-#endif
+typedef unsigned int	word32;	/* 32 bit */
 
 typedef byte*	byteptr;
 typedef word16*	word16ptr;
