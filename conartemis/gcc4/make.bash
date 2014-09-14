42fd0ede3176c2a63e9937dc4f1c9b5c7bd1db0d
John Howard <orthopteroid@gmail.com>
2014-09-13 19:17:23 -0700

    lib makefile changes

--- conartemis/gcc4/make.bash
+++ conartemis/gcc4/make.bash
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-CFLAGS="-I../../libartemis/inc/ -std=c99"
+CFLAGS="-I../../../libartemis/inc/ -std=c99"
 
 if [ "$1" == "debug" ]
+CFLAGS="$CFLAGS -O3"
 then
 CFLAGS="$CFLAGS -g -fstack-check -D _DEBUG"
 fi
 
 mkdir obj 2> /dev/null
+cp ../../libartemis/gcc4/obj/libartemis.so ./obj/libartemis.so
+cd obj
 
-echo gcc $CFLAGS -o ./obj/artemis ../src/main.c ../../libartemis/gcc4/obj/libartemis.so
-gcc $CFLAGS -o ./obj/artemis ../src/main.c ../../libartemis/gcc4/obj/libartemis.so
+echo gcc $CFLAGS -o artemis ../../src/main.c libartemis.so
+gcc $CFLAGS -o artemis ../../src/main.c libartemis.so
 
-#readelf --symbols ./obj/artemis
-
-if [ "$1" != "debug" ]
-then
-echo strip -w -K platform_* -K ar_core_* -K ar_uri_* -K test_* obj/artemis
-strip -w -K platform_* -K ar_core_* -K ar_uri_* -K test_* obj/artemis
-readelf --symbols ./obj/artemis
-fi
-
-
+cd ..
--- libartemis/gcc4/make.bash
+++ libartemis/gcc4/make.bash
@@ -4,6 +4,7 @@ CFLAGS="-fpic -I../inc/ -std=c99 -c -msse3"
 #CFLAGS="-fpic -I../inc/ -std=c99 -c"
 
 if [ "$1" == "debug" ]
+CFLAGS="$CFLAGS -O3"
 then
 CFLAGS="$CFLAGS -g -fstack-check -D _DEBUG"
 fi
@@ -11,18 +12,19 @@ fi
 AA=`find ../src -name *.c`
 
 BB=${AA//..\/src\//} # strip path
-SRC=${BB//.c/} # strip ext
+SRCS=${BB//.c/} # strip ext
 
 ZZ=${AA//..\/src/.\/obj} # swap path
 OBJS=${ZZ//.c/.o} # swap ext
 
 mkdir obj 2> /dev/null
-for i in $SRC
+for i in $SRCS
 do
 	echo gcc $CFLAGS -o ./obj/$i.o ../src/$i.c
 	gcc $CFLAGS -o ./obj/$i.o ../src/$i.c
 	if [ $? -ne 0 ] ; then break ; fi
 done
+
 echo gcc -shared -o obj/libartemis.so $OBJS
 gcc -shared -o obj/libartemis.so $OBJS
 
