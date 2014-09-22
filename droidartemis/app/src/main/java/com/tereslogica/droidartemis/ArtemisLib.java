0ccb1e643d84fac409d32d878ce66e59d4836f0b
John Howard <orthopteroid@gmail.com>
2014-09-22 14:29:29 -0700

    java class related changes

--- droidartemis/app/src/main/java/com/tereslogica/droidartemis/ArtemisLib.java
+++ droidartemis/app/src/main/java/com/tereslogica/droidartemis/ArtemisLib.java
@@ -10,11 +10,13 @@ public class ArtemisLib {
     public native String nativeGetString();
     public native boolean nativeGetStatusOK();
     public native String nativeDecode( String data );
-    //public native String nativeDecodeB64( String data );
+
     public native int[] nativeShareInfo( String share ); // 0 = shares, 1 = threshold
-    public native String nativeShareField( String share, String field, int num );
     public native String nativeShareClue( String share );
     public native String nativeShareLocation( String share );
+
+    //public native String nativeDecodeB64( String data );
+    //public native String nativeShareTopic( String share, String field, int num );
 
     ArtemisLib() {
         String sss = nativeGetString();
