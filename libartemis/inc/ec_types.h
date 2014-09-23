a9632225fb427004d76cddfecba01f6987728fed
john howard <orthopteroid@gmail.com>
2014-09-23 13:24:24 -0700

    added constbyteptr type

--- libartemis/inc/ec_types.h
+++ libartemis/inc/ec_types.h
@@ -1,11 +1,12 @@
 #ifndef PW_TYPES_H
 #define PW_TYPES_H
 
-typedef unsigned char	byte;	/*  8 bit */
-typedef unsigned short	word16;	/* 16 bit */
-typedef unsigned int	word32;	/* 32 bit */
+typedef unsigned char		byte;	/*  8 bit */
+typedef unsigned short		word16;	/* 16 bit */
+typedef unsigned int		word32;	/* 32 bit */
 
-typedef byte*	byteptr;
-typedef word16*	word16ptr;
+typedef byte*		byteptr;
+typedef const byte*	constbyteptr;
+typedef word16*		word16ptr;
 
 #endif // PW_TYPES_H
