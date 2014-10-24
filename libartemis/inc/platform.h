0b0554bef71392455e6781f2eb8f3c9dad735459
John Howard <orthopteroid@gmail.com>
2014-10-23 18:06:34 -0700

    refactoring of platform ifdefs

--- libartemis/inc/platform.h
+++ libartemis/inc/platform.h
@@ -8,30 +8,119 @@
 //#define ENABLE_TESTFAIL
 //#define SHOW_FAILURES
 
-#if defined(_WINDOWS)
-
-	#define _CRTDBG_MAP_ALLOC
-	#include <stdlib.h>
-	#include <crtdbg.h>
-
-	#define PRAGMA_SOMETHING __pragma("something")
-	#define PRAGMA_PUSH
-	#define PRAGMA_POP
-	#define PRAGMA_O3
-
-	#if defined(_DEBUG)
-		#define DEBUGPRINT(f,v) printf(f,(v))
+/////////////////////////////
+#if !defined(PLATFORM_PRAGMAS)
+	#define PLATFORM_PRAGMAS
+	#if defined(_WINDOWS)
+		#define PRAGMA_SOMETHING __pragma("something")
+		#define PRAGMA_PUSH
+		#define PRAGMA_POP
+		#define PRAGMA_O3
+	#elif defined(__ANDROID__)        
+		#define PRAGMA_PUSH
+		#define PRAGMA_POP
+		#define PRAGMA_O3
 	#else
-		#define DEBUGPRINT(f,v) (0)
+		#define PRAGMA_PUSH _Pragma("GCC push_options")
+		#define PRAGMA_POP _Pragma("GCC pop_options")
+		#define PRAGMA_O3 _Pragma("GCC optimize(\"O3\")")
 	#endif
+#endif
 
-	#if !defined(LITTLE_ENDIAN) && !defined(BIG_ENDIAN)
+/////////////////////////////
+#if !defined(LITTLE_ENDIAN) && !defined(BIG_ENDIAN)
+	#if defined(_WINDOWS)
 		#if defined(_M_IX86) || defined(_M_AMD64) || defined(_M_I86) || defined(__alpha)
 			#define LITTLE_ENDIAN
 		#else
 			#define BIG_ENDIAN
 		#endif
+	#else
+		#if defined(__BYTE_ORDER__)
+			#if (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+				#define BIG_ENDIAN
+			#else
+				#define LITTLE_ENDIAN
+			#endif
+		#else
+			int CompilerEndianProblems[-1];
+		#endif
 	#endif
+#endif
+
+/////////////////////////////
+#if !defined(DEBUGPRINT)
+	#if defined(_DEBUG)
+		#if defined(__ANDROID__)
+			#include <android/log.h>
+			#define DEBUGPRINT(...) __android_log_print(ANDROID_LOG_INFO, "libartemis", __VA_ARGS__ );
+		#else
+			#define DEBUGPRINT(...) printf(__VA_ARGS__)
+		#endif
+	#else
+		#define DEBUGPRINT(...) (0)
+	#endif
+#endif
+
+/////////////////////////////
+#if !defined(DEBUGHALT)
+	#if defined(_DEBUG)
+		#if defined(_WINDOWS)
+			#define DEBUGHALT __debugbreak()
+		#else
+			#include <assert.h>
+			#define DEBUGHALT assert(0)
+		#endif
+	#else
+		#define DEBUGPRINT (0)
+	#endif
+#endif
+
+/////////////////////////////
+#if !defined(ASSERT)
+	#if defined(_DEBUG)
+		#if defined(__ANDROID__)
+			#define ASSERT( c ) do { if( !(c) ) { DEBUGPRINT("ASSERT Failed %s line %d", __FILE__, __LINE__ ); DEBUGHALT; } } while(0)
+		#else
+			#define ASSERT( c ) do { if( !(c) ) { DEBUGHALT; } } while( 0 )
+		#endif
+	#else
+		#define ASSERT( c ) (0)
+	#endif
+#endif
+
+// TODO: rip
+#if !defined(TESTASSERT)
+	#define TESTASSERT(c) ASSERT(c)
+#endif
+
+/////////////////////////////
+#if !defined(LOGFAIL)
+	#if defined(_DEBUG)
+		int ar_util_istest();
+		const char* ar_util_rclookup(int);
+		#if defined(__ANDROID__)
+			// halt not allowed on android
+			#define LOGFAIL(c) do { DEBUGPRINT("LOGFAIL %s at %s line %d\n", ar_util_rclookup(c), __FILE__, __LINE__); } while( 0 )
+		#else
+			#if defined(SHOW_FAILURES)
+				#define LOGFAIL(c) do { if( !ar_util_istest() ) { DEBUGHALT; } else DEBUGPRINT("LOGFAIL %s at %s line %d\n", ar_util_rclookup(c), __FILE__, __LINE__); } while( 0 )
+			#else
+				#define LOGFAIL(c) do { if( !ar_util_istest() ) { DEBUGHALT; } } while( 0 )
+			#endif
+		#endif
+	#else
+		#define LOGFAIL(c) (0)
+	#endif
+#endif
+
+///////////////////
+
+#if defined(_WINDOWS)
+
+	#define _CRTDBG_MAP_ALLOC
+	#include <stdlib.h>
+	#include <crtdbg.h>
 
 	#include <crtdefs.h>
 
@@ -41,37 +130,9 @@
 	//#define DLLDECL __declspec(dllimport)
 	//#endif // WINDLL_EXPORTS
 
+	// TODO: remove?
 	#if defined(_DEBUG)
 		DLLDECL char* RC_LOOKUP( int rc );
-	#endif
-
-	#if !defined(LOGFAIL)
-		#if defined(_DEBUG)
-			#if defined(SHOW_FAILURES)
-				#define LOGFAIL(c) do { if( !ar_util_istest() ) { __debugbreak(); } else printf("LOGFAIL %s at %s line %d\n", ar_util_rclookup(c), __FILE__, __LINE__); } while( 0 )
-			#else
-				#define LOGFAIL(c) do { if( !ar_util_istest() ) { __debugbreak(); } } while( 0 )
-			#endif
-		#else // debug
-			#define LOGFAIL(c) (0)
-		#endif // debug
-	#endif
-
-	#if !defined(ASSERT)
-		#if defined(_DEBUG)
-			#include <assert.h>
-			#define ASSERT( c ) do { if( !(c) ) { __debugbreak(); } } while( 0 )
-		#else // debug
-			#define ASSERT( c ) (0)
-		#endif // debug
-	#endif
-
-	#if !defined(TESTASSERT)
-		#if defined(_DEBUG)
-			#define TESTASSERT( c ) do { if( !(c) ) { __debugbreak(); } } while( 0 )
-		#else // debug
-			#define TESTASSERT( c ) (0)
-		#endif // debug
 	#endif
 
 	#if !defined(strdup)
@@ -84,80 +145,9 @@
 
 #else // win
 
-    #if defined(__ANDROID__)
-        #include <android/log.h>
-        
-        #if defined(NDK_DEBUG) && !defined(_DEBUG)
-            #define _DEBUG
-        #endif
-
-        #define PRAGMA_PUSH
-        #define PRAGMA_POP
-        #define PRAGMA_O3
-    #else
-        #define PRAGMA_PUSH _Pragma("GCC push_options")
-        #define PRAGMA_POP _Pragma("GCC pop_options")
-        #define PRAGMA_O3 _Pragma("GCC optimize(\"O3\")")
-    #endif
-
-	#if defined(_DEBUG)
-		#if defined(__ANDROID__)
-			#define DEBUGPRINT(f, v) __android_log_print(ANDROID_LOG_INFO, "libartemis", (f), (v) );
-		#else
-			#define DEBUGPRINT(f,v) printf(f,(v))
-		#endif
-	#else
-		#define DEBUGPRINT(f,v) (0)
-	#endif
-	
-	#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
-		#define BIG_ENDIAN
-	#else
-		#define LITTLE_ENDIAN
-	#endif
-
 	#include <stddef.h>
 
 	#define DLLDECL
-
-	int ar_util_istest();
-
-	#if !defined(LOGFAIL)
-		#if defined(NDK_DEBUG)
-			#define LOGFAIL(c) __android_log_print(ANDROID_LOG_INFO, "libartemis", "LOGFAIL %s at %s line %d", ar_util_rclookup(c), __FILE__, __LINE__ )
-		#elif defined(_DEBUG)
-			#if defined(SHOW_FAILURES)
-				#include <assert.h>
-				#define LOGFAIL(c) do { printf( "LOGFAIL %s at %s line %d\n", ar_util_rclookup(c), __FILE__, __LINE__ ); if( !ar_util_istest() ) { assert(0); } } while(0)
-			#else
-				#include <assert.h>
-				#define LOGFAIL(c) do { if( !ar_util_istest() ) { printf( "LOGFAIL %s at %s line %d\n", ar_util_rclookup(c), __FILE__, __LINE__ ); assert(0); } } while(0)
-			#endif
-		#else // debug
-			#define LOGFAIL(c) (0)
-		#endif // debug
-	#endif
-
-	#if !defined(ASSERT)
-		#if defined(NDK_DEBUG)
-			#include <assert.h>
-			#define ASSERT( c ) do { if( !(c) ) { __android_log_print(ANDROID_LOG_INFO, "libartemis", "ASSERT Failed %s line %d", __FILE__, __LINE__ ); assert(0); } } while(0)
-		#elif defined(_DEBUG)
-			#include <assert.h>
-			#define ASSERT( c ) assert( c )
-		#else // debug
-			#define ASSERT( c ) (0)
-		#endif // debug
-	#endif
-
-	#if !defined(TESTASSERT)
-		#if defined(_DEBUG)
-			#include <assert.h>
-			#define TESTASSERT( c ) assert( c )
-		#else // debug
-			#define TESTASSERT( c ) (0)
-		#endif // debug
-	#endif
 
 	#define max(a,b) ((a)>(b)?(a):(b))
 	#define memcpy_s(a,b,c,d) memcpy(a,c,d)
