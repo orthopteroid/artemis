2dbeb853e699e2aaf7b29feab40df5b08bc2cb5e
John Howard <orthopteroid@gmail.com>
2014-10-23 18:14:15 -0700

    code consolidations and button formatting

--- droidartemis/app/src/main/java/com/tereslogica/droidartemis/TopicListActivity.java
+++ droidartemis/app/src/main/java/com/tereslogica/droidartemis/TopicListActivity.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -185,8 +184,7 @@ public class TopicListActivity extends FragmentActivity {
     }
 
     public void onClickNew(View view) {
-        Intent i = new Intent(getApplicationContext(), TopicCreatorActivity.class);
-        startActivity(i);
+        startActivity( new Intent(getApplicationContext(), TopicCreatorActivity.class) );
     }
 
     public void onClickScan(View v) {
