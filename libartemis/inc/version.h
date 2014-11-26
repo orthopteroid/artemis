b21f3aece77ce6d6eb4d3fdf63e81a527165fb1f
john howard <orthopteroid@gmail.com>
2014-11-26 12:34:20 -0800

    core: unit #define cleanups

--- libartemis/inc/version.h
+++ libartemis/inc/version.h
@@ -3,15 +3,17 @@
 #include "ec_param.h"
 
 // units are 16-bit words
-#define AR_UNITS ((GF_K*GF_L + 15)/16 + 1) // 9 units = 136 gf_m
+#define AR_UNITS ((GF_K*GF_L + 15)/16 + 1)
 
-// property-length (in 16bit words)
-#define AR_VERIFYUNITS		1 // 16 bits
+// unit length is 16 bits
+// 9 units (144 bits) ie. 136 gfm
+// 7 units (112 bits) ie. 91 gfm
+// 6 units (92 bits)
+
 #define AR_MACUNITS			4 // 64 bits
-
-#define AR_CRYPTKEYUNITS	AR_UNITS-1 // 8 units @ 136 gf_m
-#define AR_SESSKEYUNITS		AR_UNITS-1 // 8 units @ 136 gf_m
-#define AR_PRIVKEYUNITS		AR_UNITS-1 // 8 units @ 136 gf_m
+#define AR_CRYPTKEYUNITS	AR_UNITS-1
+#define AR_SESSKEYUNITS		AR_UNITS-1
+#define AR_PRIVKEYUNITS		AR_UNITS-1
 
 #define AR_LOCSTR	"arcanacard.com"
 #define AR_LOCHASH	0x1E
